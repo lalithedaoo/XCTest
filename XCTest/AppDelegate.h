806699d35e98b98277527c89225bf0c873527d7a
Lalit Hedaoo <lalit.hedaoo@gmail.com>
2014-07-07 17:17:55 +0530

    adding project file

---
+++ XCTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XCTest
+//
+//  Created by Lalit Hedaoo on 07/07/14.
+//  Copyright (c) 2014 Lalit Hedaoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XCTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XCTest
+//
+//  Created by Lalit Hedaoo on 07/07/14.
+//  Copyright (c) 2014 Lalit Hedaoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
